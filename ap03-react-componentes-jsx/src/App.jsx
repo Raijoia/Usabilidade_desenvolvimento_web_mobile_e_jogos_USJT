874c5f83f1000dd3cc24ebdeba0b28eec7ddc11e
Rai Joia <82318841@ulife.com.br>
2025-08-28 20:49:27 -0300

    ap03(feat): add project react

---
+++ ap03-react-componentes-jsx/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <label htmlFor="nome">Nome:</label>
+      <input type="text" id="nome" />
+      <button>Enviar</button>
+    </div>
+  )
+}
+
+export default App
